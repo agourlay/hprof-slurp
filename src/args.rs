d42d046f008a065bf1727b2fd39b3f4333835ebe
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2021-12-16 11:18:31 +0100

    next version

--- src/args.rs
+++ src/args.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 pub fn get_args() -> Result<(String, usize, bool, bool), HprofSlurpError> {
     let matches = App::new("hprof-slurp")
-        .version("0.2.0")
+        .version("0.2.1")
         .author("Arnaud Gourlay <arnaud.gourlay@gmail.com>")
         .about("JVM heap dump hprof file analyzer")
         .arg(
