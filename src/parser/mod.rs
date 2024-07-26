d4e598629d431ae169beb39aa3a26f2f6865bc97
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2024-07-26 14:55:28 +0200

    happy clippy - keeping on parsing everything even if not used after

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,6 +1,8 @@
 pub mod file_header_parser;
+#[allow(dead_code)]
 pub mod gc_record;
 mod primitive_parsers;
+#[allow(dead_code)]
 pub mod record;
 pub mod record_parser;
 pub mod record_stream_parser;
